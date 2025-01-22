781d3e6758fae334a6385d6a09603c094e48fee8
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2025-01-22 14:33:12 -0500

    Method created to import String to Food object on ID

--- Solutions/Java/src/main/java/Kata.java
+++ Solutions/Java/src/main/java/Kata.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Kata {
     public String hello(String name) {
@@ -5,7 +10,36 @@ public class Kata {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        Kata kata = new Kata();
+        List<Food> foods = new ArrayList<Food>();
+
+        File file = new File("dataset.csv");
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                for (String foodData : line.split(",")) {
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("End");
+    }
+
+    public static Food importData(String data) {
+        Food food  = new Food();
+        String[] foodData = data.split(",");
+        if (foodData.length == 5) {
+            if(foodData[0].matches("\\d+")) {
+                food.setId(Integer.parseInt(foodData[0]));
+            } else {
+                System.out.println(foodData[0]);
+                System.out.println("Invalid ID");
+                return null;
+            }
+            return food;
+        }
+        System.out.println("Not Enough Data Points");
+        return null;
     }
 }
