acd08749ad8bd70344c59ccf8fc03d053c606c30
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2025-01-22 14:33:37 -0500

    Created test to test import ID from string in Kata class

--- Solutions/Java/src/test/java/KataTest.java
+++ Solutions/Java/src/test/java/KataTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class KataTest {
     private Kata fixture;
@@ -16,6 +17,14 @@ public class KataTest {
         String name = "Ingage";
         assertEquals(String.format("Hello %s", name), fixture.hello(name));
     }
+
+    @Test
+    public void importFoodFromString() {
+        String foodData = "1,Fruit,Apple,74.41,2024-01-23T06:58:00";
+        Food food = Kata.importData(foodData);
+        assertNotEquals(food, null);
+        assertEquals(food.getId(), 1);
+   }
 
 
 }
