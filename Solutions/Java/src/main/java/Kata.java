39178f2af00a19171ae4fa7b056d8d382eaba772
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2025-01-22 14:53:44 -0500

    Import method complete for simple data

--- Solutions/Java/src/main/java/Kata.java
+++ Solutions/Java/src/main/java/Kata.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -33,13 +34,33 @@ public class Kata {
             if(foodData[0].matches("\\d+")) {
                 food.setId(Integer.parseInt(foodData[0]));
             } else {
-                System.out.println(foodData[0]);
-                System.out.println("Invalid ID");
+                return null;
+            }
+            if (foodData[1].matches("[A-Za-z ]+")) {
+                food.setCategory(foodData[1]);
+            } else {
+                return null;
+            }
+            if (foodData[2].matches("[A-Za-z ]+")) {
+                food.setSubcategory(foodData[2]);
+            } else {
+                return null;
+            }
+            if (foodData[3].matches("\\d+.\\d+")) {
+                food.setValue(Double.parseDouble(foodData[3]));
+            } else {
+                return null;
+            }
+            if (foodData[4].matches(
+                    "([0-9]{4})-?(1[0-2]|0[1-9])-?(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):" +
+                            "?([0-5][0-9]):?([0-5][0-9])")) {
+                food.setDate(LocalDateTime.parse(foodData[4]));
+            } else {
+                System.out.println("Invalid date");
                 return null;
             }
             return food;
         }
-        System.out.println("Not Enough Data Points");
         return null;
     }
 }
